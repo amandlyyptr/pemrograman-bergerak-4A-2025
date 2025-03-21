5f03542aa21009ba3fe72ff4cdaf267693bd86bd
amandlyyptr <amandaaulia867@gmail.com>
2025-03-21 18:43:02 +0700

    230441100017_AmandaAulia_KakKukuh

---
+++ tugas-dart/23_017_amanda_aulia_putri/bin/exam4/exam4.dart
@@ -0,0 +1,29 @@
+class PraktikanStudent {
+  String fullName;
+  int age;
+
+  PraktikanStudent(this.fullName, this.age);
+
+  int incrementAge() {
+    // TODO 1
+
+    return age + 1;
+
+    // End of TODO 1
+  }
+
+  Future<String> getStudentInfo() async {
+    // TODO 2
+    await Future.delayed(Duration(seconds: 3));
+    return Future.value("Nama Lengkap: $fullName, Umur: $age tahun");
+
+    // End of TODO 2
+  }
+}
+
+dynamic createStudent() {
+  // TODO 3
+  return PraktikanStudent("Amanda Aulia", 19);
+
+  // End of TODO 3
+}
